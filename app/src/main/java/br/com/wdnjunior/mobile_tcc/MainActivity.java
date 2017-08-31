8003c51fd23f96358028c7dffbaa0b132d972c0e
Wilson Donizetti <wilsondonizetti@hotmail.com>
2017-08-31 14:40:51 -0300

    funcionando

--- app/src/main/java/br/com/wdnjunior/mobile_tcc/MainActivity.java
+++ app/src/main/java/br/com/wdnjunior/mobile_tcc/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebChromeClient;
 import android.app.Activity;
 import android.webkit.WebViewClient;
 import android.net.Uri;
@@ -17,36 +18,41 @@ public class MainActivity extends Activity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.loadUrl("http://tcc.wilsondonizetti.eti.br");
-        myWebView.setWebViewClient(new MyWebViewClient());
-
-        WebSettings webSettings = myWebView.getSettings();
-
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.clearCache(true);
+        webView.clearHistory();
+        WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setBuiltInZoomControls(true);
+        //webSettings.setSupportZoom(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setGeolocationEnabled(true);
+        webSettings.setSavePassword(true);
+        webSettings.setSaveFormData(true);
+        webSettings.setSupportZoom(false);
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setSupportMultipleWindows(false);
+        webView.loadUrl("http://tcc.wilsondonizetti.eti.br");
 
-
-
-
+        setContentView(webView);
 
     }
     // Use When the user clicks a link from a web page in your WebView
     private class MyWebViewClient extends WebViewClient {
-
         @Override
-
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
-
-            if (Uri.parse(url).getHost().equals("wilsondonizetti.eti.br")) {
-
+            if (Uri.parse(url).getHost().equals("tcc.wilsondonizetti.eti.br")) {
+                // This is my web site, so do not override; let my WebView load the page
                 return false;
-
             }
-
+            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
             Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-
             startActivity(intent);
-
             return true;
         }
     }
